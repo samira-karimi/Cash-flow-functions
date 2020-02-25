c8e94051446178d835e42a04bb0f8ae5a2fd1ac4
Samira <55062125+samira-karimi@users.noreply.github.com>
2020-02-25 00:04:46 -0600

    Update cashflows.java

--- cashflows.java
+++ cashflows.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hw1;
+package cashflows;
     import jsl.utilities.random.distributions.Normal;
     import jsl.utilities.statistic.Statistic;
 /**
