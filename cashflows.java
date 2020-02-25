35bbafabde0a3c5695ed927b6e85d9909e249474
Samira <55062125+samira-karimi@users.noreply.github.com>
2020-02-25 00:04:06 -0600

    Update cashflows.java

--- cashflows.java
+++ cashflows.java
@@ -10,34 +10,21 @@ package hw1;
  *
  * @author Samira
  */
-public class hw1 {
+public class cashflows {
 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("P given F is: "+getPgivenF(0.1,5,100)); //1-a
-        System.out.println("P given A is: "+getPgivenA(0.1,5,100)); //1-b
-        System.out.println("F given P is: "+getFgivenP(0.1,5,100)); //1-c
-        System.out.println("A given P is: "+getAgivenP(0.1,5,100)); //1-d
-        System.out.println("F given A is: "+getFgivenA(0.1,5,100)); //1-e
-        System.out.println("A given F is: "+getAgivenF(0.1,5,100)); //1-f
-        
-        
-        double[] Test={-4000,400,400,400,400}; //2
-        CashFlow CF=new CashFlow(0.1, Test);   //2
-        System.out.println("The present worth of our test cash flow for question 2 is: "+CF.getPresentWorth());  //2
-        
-        
-//        double r = 0.10;
-//        double[] y = {-5000.0, 1400.0, 4100.0, 4000.0, 200.0};
-//        CashFlow c = new CashFlow(r, y);
-//        System.out.println("Question 6's answer is: "+c.getAnnualWorth());
-        
-        
-        
+
+        System.out.println("P given F is: "+getPgivenF(0.1,5,100)); 
+        System.out.println("P given A is: "+getPgivenA(0.1,5,100)); 
+        System.out.println("F given P is: "+getFgivenP(0.1,5,100)); 
+        System.out.println("A given P is: "+getAgivenP(0.1,5,100)); 
+        System.out.println("F given A is: "+getFgivenA(0.1,5,100)); 
+        System.out.println("A given F is: "+getAgivenF(0.1,5,100)); 
+  
         
         Normal normRnd = new Normal(2, Math.pow(1.5, 2)); //3
         Statistic SampleCollector = new Statistic();
@@ -48,22 +35,13 @@ public class hw1 {
             ProbabilityCollector.collect(v > 1);
         }
         
-//        for (int k = 0; k < 1000; k++) {
-//            double nn = normRnd.getValue();
-//            SampleCollector.collect(nn);
-//            if (nn > 1) {
-//                ProbabilityCollector.collect(true);
-//            } else {
-//                ProbabilityCollector.collect(false);
-//            }
-//        }
 
         System.out.println("The sample size is: " + SampleCollector.getCount());
         System.out.println("The sample mean is: " + SampleCollector.getAverage());
         System.out.println("The sample Standars Deviation is: " + SampleCollector.getStandardDeviation());
         System.out.println("The sample minimum is: " + SampleCollector.getMin());
         System.out.println("The sample maximum is: " + SampleCollector.getMax());
-        System.out.println("The sample %99 confidence interval is: " + SampleCollector.getConfidenceInterval(0.99));
+        System.out.println("The sample %90 confidence interval is: " + SampleCollector.getConfidenceInterval(0.90));
         System.out.println("95% confidence interval for P(x>1) is: " + ProbabilityCollector.getConfidenceInterval(0.95));
         
     
